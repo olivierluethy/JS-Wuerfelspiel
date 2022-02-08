a3560cfa3a6050bfc395df9e3b5508184b2b6c60
Oli7000 <olivier.luethy@gmx.net>
2022-02-08 13:58:56 +0100

    Ausgangslage

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const lotto = document.querySelector('#lotto');
-    lotto.addEventListener('input', lottoNumberChanged);
-
-    function lottoNumberChanged(event) {
-        let chosenNumbers = lotto.querySelectorAll(
-            'input[type=checkbox]:checked');
-        if (chosenNumbers.length > 6) {
-            event.target.checked = false;
-            alert('Mit sieben Zahlen wird Ihr Tippschein ungültig!');
-        }
-    }
+    
 });
