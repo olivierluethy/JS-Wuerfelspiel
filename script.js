6353da67d3814f33ff88984b6e220ab01ac3041a
Oli7000 <olivier.luethy@gmx.net>
2022-02-11 08:26:09 +0100

    Aufgabe gelöst

--- script.js
+++ script.js
@@ -1,3 +1,72 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-});
+let counter = 0;
+let betsNumber = [];
+let lotto = [];
+let equal = 0;
+let bets = 6;
+
+function lottoNumber(value) {
+    if (counter == 6) {
+
+    } else {
+        betsNumber.push(value);
+        var myBets = document.getElementById("myBets");
+        var p = document.createElement("p");
+        p.appendChild(document.createTextNode(value));
+        myBets.appendChild(p);
+        bets--;
+        document.querySelector("h2").innerHTML = "Please chose " + bets + " out of 49";
+        counter++;
+
+        if (counter == 6) {
+            let lotto1 = Math.floor(Math.random() * 49);
+            let lotto2 = Math.floor(Math.random() * 49);
+            let lotto3 = Math.floor(Math.random() * 49);
+            let lotto4 = Math.floor(Math.random() * 49);
+            let lotto5 = Math.floor(Math.random() * 49);
+            let lotto6 = Math.floor(Math.random() * 49);
+
+            lotto.push(lotto1);
+            lotto.push(lotto2);
+            lotto.push(lotto3);
+            lotto.push(lotto4);
+            lotto.push(lotto5);
+            lotto.push(lotto6);
+
+            var winners = document.getElementById("winners");
+            var p = document.createElement("p");
+            p.appendChild(document.createTextNode(lotto1));
+            winners.appendChild(p);
+
+            var p = document.createElement("p");
+            p.appendChild(document.createTextNode(lotto2));
+            winners.appendChild(p);
+
+            var p = document.createElement("p");
+            p.appendChild(document.createTextNode(lotto3));
+            winners.appendChild(p);
+
+            var p = document.createElement("p");
+            p.appendChild(document.createTextNode(lotto4));
+            winners.appendChild(p);
+
+            var p = document.createElement("p");
+            p.appendChild(document.createTextNode(lotto5));
+            winners.appendChild(p);
+
+            var p = document.createElement("p");
+            p.appendChild(document.createTextNode(lotto6));
+            winners.appendChild(p);
+
+            for (var i = 0; i < lotto.length; i++) {
+                if (lotto[i] == myBets[i]) {
+                    equal++;
+                }
+            }
+            if (equal == 6) {
+                document.querySelector("body").style.backgroundColor = "green";
+            } else {
+                alert("You Lost!");
+            }
+        }
+    }
+}
